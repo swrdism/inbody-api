3acb2eea1f4d31f0b42ac644b803dcfdff81de4b
Yi-Ming Zhuang <swrdism@gmail.com>
2021-01-28 00:54:25 +0800

    導入Spring-Boot (#1)

---
+++ src/main/java/io/github/swrdism/InbodyApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.swrdism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InbodyApiApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(InbodyApiApplication.class, args);
+    }
+}
